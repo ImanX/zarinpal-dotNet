f02feec4b75952edfa39ecdfb3c5a57f511d8cea
ImanX <imannamix@gmail.com>
2019-01-22 15:02:54 +0330

    a few changes

--- ZarinPal/Models/VerificationResponse.cs
+++ ZarinPal/Models/VerificationResponse.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Web;
 namespace ZarinPal
 {
     public class VerificationResponse
     {
+
+        public bool IsSuccess { get; set; }
+        public String Authority { get; set; }
+
+
+
+        public VerificationResponse()
+        {
+            this.IsSuccess =
+        }
     }
 }
